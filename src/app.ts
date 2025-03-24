0e8aca09d5b0be6058db723a0a42eddee089c5d6
Everth Paredes <fpa090416@gmail.com>
2025-03-23 21:58:24 -0600

    Opciones de Yargs

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,12 @@
 import { yarg } from "./config/plugins/args.plugin";
 
-console.log(process.argv)
-console.log(yarg.b)
+// console.log(process.argv)
+// console.log(yarg.b)
+
+(async() => {
+    await main()
+})();
+
+async function main() {
+    console.log(yarg)
+}
