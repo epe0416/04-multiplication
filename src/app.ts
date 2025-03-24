67b1e66d8097d9a08edb955ab34b3311218b6903
Everth Paredes <fpa090416@gmail.com>
2025-03-23 21:44:51 -0600

    Add Yargs

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,4 @@
-import fs from 'fs';
+import { yarg } from "./config/plugins/args.plugin";
 
-let outputMessage = '';
-const base = 5;
-const headerMessage = `
-==================================
-        Tabla del ${base}
-================================== \n
-`;
-
-for( let i = 1; i <= 10; i++ ) {
-    outputMessage += `${base} x ${i} = ${base * i}\n`;
-}
-
-outputMessage = headerMessage + outputMessage;
-console.log(outputMessage);
-
-const outputPath = `outputs`;
-
-fs.mkdirSync(outputPath, {recursive: true});
-fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, outputMessage);
-console.log('File created!');
+console.log(process.argv)
+console.log(yarg.b)
