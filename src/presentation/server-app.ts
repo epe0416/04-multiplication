b2fb9096ec070451074fe6c548d8f09efef4c99e
Everth Paredes <fpa090416@gmail.com>
2025-04-10 21:01:53 -0600

    SaveFile - UseCase

--- src/presentation/server-app.ts
+++ src/presentation/server-app.ts
@@ -1,4 +1,5 @@
 import { CreateTable } from "../domain/use-cases/create-table.use-case";
+import { SaveFile } from "../domain/use-cases/save-file.use-case";
 
 interface RunOptions {
     base: number,
@@ -12,7 +13,12 @@ export class ServerApp {
         console.log('Server runnign...')
         
         const table = new CreateTable().execute({ base, limit });
-
-        if(showTable) console.log(table)
+        const wasCreated = new SaveFile()
+            .execute({ fileContent: table});
+        
+        if(showTable) console.log(table);
+        (wasCreated)
+            ? console.log('File Created!')
+            : console.error('File not created!');
     }
 }
